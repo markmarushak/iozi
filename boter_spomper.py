a825b2cf6614f4408f91277ad6e3e49d3916e647
Your Name <you@example.com>
2018-12-31 15:11:28 +0300

    repo - text

--- boter_spomper.py
+++ boter_spomper.py
@@ -1,33 +1,37 @@
 import vk_api
-
-vk_session = vk_api.VkApi('markk.99@mail.ru', '12moroz12')
+vk_session = vk_api.VkApi('79056363387', 'TU3IIU')
 vk_session.auth()
 vk = vk_session.get_api()
 
+m_search = ['жизнь', 'знакомства', 'холостяк', 'прибыль', 'незнакомец', 'заработок']
+report = open('report.txt', 'a+')
+
 def addComment(post) :
-    print(post)
-    for p in post :
-        vk.wall.createComment(owner_id=p.get(1),post_id=p.get(0), message='очень круто')
+    for id in post :
+        print(id)
+        report.write('https://vk.com/'+id.get(2))
+        vk.wall.createComment(post_id=id, message='очень круто')
+        
 
 def postID(groups) :
-    id_post=[]
-    for grop in groups :
-        data = vk.wall.get(owner_id=-grop, count=2, offset=0)
-        item = data['items']
-        for k in item :
-            id_post.append({0:k.get('id'),1:k.get('owner_id')})
-
-    return addComment(id_post)            
-                               
-
-''' здесь все работает - это поиск груп по запросу НИШТЯК ОЧЕНЬ ПРИЯТНО НАБЛЮДАТЬ '''                          
+    id_post = []
+    for gr in groups :
+        data = vk.wall.get(owner_id=-gr.get(0), count=10, offset=0)
+        for key in data['items'] :
+            id_post.append({0: key.get('owner_id'), 1: key.get('id'), 2: gr.get(1)})
+    addComment(id_post)
+                          
 def searchGroupID(word) :
+    spm_gr = []
     id_group = []
-    search = vk.groups.search(q=word, count=3, offset=0)
+    search = vk.groups.search(q=word, count=10, offset=80)
     for key in search['items'] :
-        for name, data in key.items() :
-            if name == 'id' :
-                id_group.append(data)
-    return postID(id_group)
+        if(key.get('is_closed') == 0) :
+            d_post = vk.wall.get(owner_id=-key.get('id'), count=1, offset=0)
+            if(d_post['items'][0].get('comments').get('count') > 0) :                
+                id_group.append({0:key.get('id'), 1: key.get('screen_name')})
+    postID(id_group)
+            
     
-searchGroupID('жизнь')
+for s in m_search :
+    searchGroupID(s)
